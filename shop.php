03f488aed0b152ddfadda775184dddc64e1f398c
fahis1 <fahisthayyil709@gmail.com>
2022-08-19 17:50:25 +0530

    fixed repeating cart item

--- shop.php
+++ shop.php
@@ -1,21 +1,22 @@
 
 <?php
 include("include/connect.php");
-$count=0;
 if(!isset($_SESSION["cart"])) 
 { 
   $_SESSION["cart"]=array();
+  $_SESSION['count']=0;
 } 
 if(isset($_GET['ATC']))
 {
     $pid=$_GET["ATC"];
     $_SESSION["cart"][] = $pid;
-    $count=count($_SESSION["cart"]);
+    $_SESSION['count']=count($_SESSION["cart"]);
+    header("location:shop.php");
 }
 if (isset($_POST["clear_cart"]))
 {
   $_SESSION["cart"]=array();
-  $count=0;
+  $_SESSION['count']=0;
 }
 ?>
 
@@ -71,14 +72,14 @@ if (isset($_POST["clear_cart"]))
         <div class="indicator">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
           <?php
-          echo '<span class="badge badge-sm indicator-item">',$count,'</span>';
+          echo '<span class="badge badge-sm indicator-item">',$_SESSION['count'],'</span>';
           ?>
         </div>
       </label>
       <div tabindex="0" class="mt-3 card card-compact dropdown-content w-max bg-base-100 shadow">
         <div class="card-body">
         <?php 
-          echo "<span class='font-bold text-lg'>",$count," Items</span>";
+          echo "<span class='font-bold text-lg'>",$_SESSION['count']," Items</span>";
           $total=0;
           $n=0;
           $quantity=array_count_values($_SESSION['cart']);
